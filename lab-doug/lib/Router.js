9c98d039ef299ab523514577b124939c2931b646
Doug Popadince <gsmatth@icloud.com>
2016-05-19 14:36:11 -0700

    added first GET route for /api/matchscore

--- lab-doug/lib/Router.js
+++ lab-doug/lib/Router.js
@@ -1,8 +1,9 @@
-'use strict'
+'use strict';
 
 const parseUrl = require('url').parse;
 
 const Router = module.exports = function(){
+  //object that stores routes
   this.routes = {
     GET: {},
     PUT: {},
@@ -10,24 +11,24 @@ const Router = module.exports = function(){
     DELETE: {}
   };
 };
-//register routes
+//methods used to register routes
 Router.prototype.get = function(endpoint, callback){
-  this.routes,GET[endpoint] = callback;
+  this.routes.GET[endpoint] = callback;
   return this;
 };
 
 Router.prototype.post = function(endpoint, callback){
-  this.routes,POST[endpoint] = callback;
+  this.routes.POST[endpoint] = callback;
   return this;
 };
 
 Router.prototype.put = function(endpoint, callback){
-  this.routes,PUT[endpoint] = callback;
+  this.routes.PUT[endpoint] = callback;
   return this;
 };
 
 Router.prototype.delete = function(endpoint, callback){
-  this.routes,DELETE[endpoint] = callback;
+  this.routes.DELETE[endpoint] = callback;
   return this;
 };
 
@@ -35,8 +36,9 @@ Router.prototype.route = function(){
   const routes = this.routes;//I do not understand why we need this
   return function(req, res){
     req.url = parseUrl(req.url);
-    if(typeof routes[req.method][req.url] === 'function'){
-      return [req.method][req.url];
+    console.log('valid function: ' , routes[req.method][req.url]);
+    if(typeof routes[req.method][req.url.pathname] === 'function'){
+      return routes[req.method][req.url.pathname](req, res);
     }
     res.writeHead(404, {'Content-Type': 'application/json'});
     res.write('not found');
--- lab-doug/server.js
+++ lab-doug/server.js
@@ -1,13 +1,22 @@
 'use strict';
-
+console.log('test');
 const http = require('http');
 const Router = require(__dirname + '/lib/Router');
 const port = process.env.PORT || 3000;
 const router = new Router();
+console.log('empty router object: ', router);
 
+//register this route on router object constructed above
+router.get('/api/matchscore', function(req, res){
+  res.writeHead(200);
+  res.write('you reached the home page');
+  res.end();
+});
+console.log('populated router object: ', router);
 
+/*the createServer is looking for a function that takes req and res as its arguments.  the router.route() returns a function that takes req and res as its arguments*/
 const server = http.createServer(router.route());
-
+  console.log('router.route() called');
 server.listen(port, function(){
   console.log('http server started up on port: ', port);
 });
