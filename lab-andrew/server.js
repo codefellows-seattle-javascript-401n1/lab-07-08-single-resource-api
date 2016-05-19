f74276658dcba1861063fe81d8a848816fdf6a0f
Andrew Watson <andrewpeterwatson@gmail.com>
2016-05-18 19:45:37 -0700

    added parse

--- lab-andrew/server.js
+++ lab-andrew/server.js
@@ -5,3 +5,16 @@ const Router = require('./lib/router');
 
 const port = process.env.PORT || 3000;
 const router = new Router();
+
+
+router.get('/', function(req, res) {
+  res.writeHead(200);
+  res.write('Router is working');
+  res.end();
+});
+
+const server = http.createServer(router.route());
+
+server.listen(port, function() {
+  console.log(`Server is running on PORT: ${port}`);
+});
