184403404e9c8c7e75e7307a7d721f4c4f9fe445
Doug Popadince <gsmatth@icloud.com>
2016-05-20 11:19:59 -0700

    renamed some files to reflect standard naming conventions

--- lab-doug/server.js
+++ lab-doug/server.js
@@ -2,19 +2,15 @@
 
 const http = require('http');
 const Router = require(__dirname + '/lib/router');
-
 const port = process.env.PORT || 3000;
 const router = new Router();
+const matchScoreRoute = require('./route/matchscore-route');
 
-//register this route on router object constructed above
-router.get('/api/matchscore', function(req, res){
-  res.writeHead(200);
-  res.write(JSON.stringify('you reached the home'));
-  res.end();
-});
-
+//register routes on router object constructed above
+matchScoreRoute(router);
 /*the createServer is looking for a function that takes req and res as its arguments.  the router.route() returns a function that takes req and res as its arguments*/
 const server = http.createServer(router.route());
+
 server.listen(port, function(){
-  console.log('http server started up on port: ', port);
+  console.log('http server started on port: ', port);
 });
