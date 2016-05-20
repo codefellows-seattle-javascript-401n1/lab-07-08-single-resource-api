352af1517a6aff944a70e4af939726eb58d4e1aa
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-05-19 20:58:01 -0700

    random save

--- lab-anthony/route/note-route.js
+++ lab-anthony/route/note-route.js
@@ -11,13 +11,11 @@ module.exports = function(router) {
       if (req.body && req.body.content){
         const note = new Note(req.body.content);
         notePool[note.id] = note;
-        console.log(notePool);
         return response(200, note)(res);
       }
       response(400, 'bad request')(res);
     })
     .get('/api/note', function(req, res) {
-      console.log('called GET');
       const note = notePool[req.url.query.id];
       if(!req.url.query.id) {
         return response(400, 'bad request')(res);
