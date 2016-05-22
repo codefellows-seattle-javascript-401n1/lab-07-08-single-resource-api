387d18a981ff703e10a6d06fa7f788e8413616f4
FazioD <faziod@uw.edu>
2016-05-22 11:55:26 -0700

    more changes

--- lab-davide/lib/router.js
+++ lab-davide/lib/router.js
@@ -1,13 +1,15 @@
 'use strict';
 
-//requirning the parse method from the url module//
-const parseUrl = require('url').parse;
+
+//returning the parse method from the url module//
+const parseUrl = require('./parse-url');
+const parseBody = require('./parse-body');
 
 //return a constructor that is going to be a new object for our route.//
 //creating a router with a var to add things to this prototype //
 const Router = module.exports = function(){
 
-//going to rout a single property on this. route going to be an object. That object
+//going to rout a single property on this. Route is going to be an object. That object
 //is goinng to have a property for GET, POST, PUT, DELETE//
   this.routes = {
     GET: {},
@@ -18,38 +20,49 @@ const Router = module.exports = function(){
 };
 
 
-//register a callback for a specific endpoint for one of the following routes.//
+//router is an object constructor that register a single property called routes. That property has an pbject literal with empty objects - GET, POST, PUT, and DELETE//
 Router.prototype.get = function(endpoint, callback){
-  this.routes.GET[endpoint] = callback;
+  this.routes.GET['/api/note'] = callback;
   return this;
 };
 
 Router.prototype.post = function(endpoint, callback){
-  this.routes.POST[endpoint] = callback;
+  this.routes.POST['/api/note'] = callback;
+  return this;
+};
+
+Router.prototype.put = function(endpoint, callback) {
+  this.routes.PUT['api/note'] = callback;
   return this;
 };
 
 Router.prototype.delete = function(endpoint, callback){
-  this.routes.POST[endpoint] = callback;
+  this.routes.DELETE['api/note'] = callback;
   return this;
 };
 
 Router.prototype.route = function(){
   const routes = this.routes;
   return function(req, res){
-    req.url = parseUrl(req.url);
-    if(typeof routes[req.method][req.url.pathname] === 'function'){
-      return routes[req.method][req.url.pathname](req, res);
-    }
-
-
-
-
-    res.writeHead(404, {
-      'Content-Type': 'application/json'
+    Promise.all([
+      parseBody(req),
+      parseUrl(req)
+    ]).then(function(){
+      if(typeof routes[req.method][req.url.pathname] === 'function') {
+        return routes[req.method][req.url.pathname](req, res);
+      }
+      fourOhFour(res);
+    }).catch(function(err){
+      fourOhFour(res);
     });
-
-    res.write('not found');
-    res.end();
   };
 };
+
+function fourOhFour(res){
+  res.writeHead(404, {
+    'Content-Type': 'application/json'
+  });
+
+  res.write(JSON.stringify('not found'));
+  res.end();
+}
