5b2b8d1af2d909014675601cfafcdd49ce44598b
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-05-22 13:09:08 -0700

    get and post methods working, still working on delete

--- lab-jacob/route/deity-route.js
+++ lab-jacob/route/deity-route.js
@@ -2,34 +2,55 @@
 
 const Deity = require('../model/deity');
 const response = require('../lib/response');
+const Storage = require('../lib/storage');
 
+const deityStorage = new Storage(`${__dirname}/../data`);
 var valhalla = {};
 
 module.exports = function(router){
   router
-  .post('/api/deity', function(req, res){
+
+  .post('/api/deity', function(req, res){ // writes and stores a post request
     if(req.body){
+      console.log('inside .post');
       const deity = new Deity(req.body.name, req.body.power);
       valhalla[deity.id] = deity;
-      return response(200, deity)(res);
-    }
-    response(400, 'bad request')(res);
+      deityStorage.createDeity('deity', deity)
+      .then(function(deity){
+        console.log('inside post.then');
+        return response(200, deity)(res);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    } else {response(400, 'bad request')(res);}
   })
-  .get('/api/deity', function(req, res){
+
+  .get('/api/deity', function(req, res){ // writes and fetches a get request
     const deity = valhalla[req.url.query.id];
     if(deity){
-      return response(200, deity)(res);
-    }
-    response(404, 'not found')(res);
+      console.log(deity);
+      deityStorage.fetchDeity('deity', deity)
+      .then(function(deity){
+        console.log('inside get method');
+        return response(200, deity)(res);
+      }).catch(function(err){
+        console.log(err);
+      });
+    } else {response(404, 'not found')(res);}
   })
-  .delete('/api/deity', function(req, res){
+
+  .delete('/api/deity', function(req, res){ // deletes data from data pool
     console.log('heres delete');
     if (req.body) {
       const deity = req.body.id;
       console.log('inside delete if');
       delete valhalla[deity];
-      return response(200, deity)(res);
-    }
-    response(404, 'nothing here')(res);
+      deityStorage.deleteDeity('deity', deity)
+      .then(function(deity){
+        return response(200, deity)(res);
+      }).catch(function(err){
+        console.log(err);
+      });
+    } else {response(404, 'nothing here')(res);}
   });
 };
