6cef5c6543c4db5321432235d044c235671419ca
Cortney Lane <cdlane124@gmail.com>
2016-05-22 09:06:49 -0700

    improve bonus /api/people/all module, start lab-08

--- lab-cortney/route/people-route.js
+++ lab-cortney/route/people-route.js
@@ -8,25 +8,35 @@ var peepPool = {}; // get it? hahahahaaahhaahaha
 module.exports = function(router){
   router
   .post('/api/people', function(req, res){
+
     if (req.body.name){
       const person = new People(req.body.name);
       peepPool[person.id] = person;
       return response(200, person)(res);
     }
     response(400, 'bad request!')(res);
+
   })
   .get('/api/people', function(req, res){
+
     const person = peepPool[req.query.id];
     if (person){
       return response(200, person)(res);
     }
     response(404, 'not found')(res);
-  }).get('/api/people/all', function(req, res){
-    if (peepPool){
-      return response(200, peepPool)(res);
+
+  })
+  .get('/api/people/all', function(req, res){
+
+    for (var peep in peepPool){
+      if (peep){
+        return response(200, peepPool)(res);
+      }
     }
-    return response(400, 'hmm, nobody here')(res);
-  }).delete('/api/people', function(req, res){
+    return response(400, 'hmm, nobody here. try POSTing a peep?')(res);
+  })
+  .delete('/api/people', function(req, res){
+
     if (req.body){
       const person = peepPool[req.body.id];
       if (person){
@@ -34,6 +44,7 @@ module.exports = function(router){
         return response(200, person)(res);
       }
       return response(404, 'not found')(res);
+
     }
   });
 
