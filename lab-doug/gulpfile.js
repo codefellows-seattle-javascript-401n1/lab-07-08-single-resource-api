d8d44307ea34f202bd8cddfda93b181e0281389d
Doug Popadince <gsmatth@icloud.com>
2016-05-23 23:56:09 -0700

    This is the initial commit for lab07/08.  All the methods and routes work for lab 07, but most tests are failing. Lab08 only has constructor and two tests.  No routes have been updated.

--- lab-doug/gulpfile.js
+++ lab-doug/gulpfile.js
@@ -5,9 +5,6 @@ const eslint = require('gulp-eslint');
 const nodemon = require('gulp-nodemon');
 const mocha = require('gulp-mocha');
 const nyan = require('karma-nyan-reporter');
-
-
-
 const paths = ['*.js', 'lib/*.js', 'model/*.js', 'test/*.js', 'route/*.js'];
 
 gulp.task('eslint', function(){
--- lab-doug/route/matchscore-route.js
+++ lab-doug/route/matchscore-route.js
@@ -5,26 +5,17 @@ const response = require('../lib/response');
 var matchScorePool = {};
 
 module.exports = function(router){
-  // console.log('entered matchscore-route.route');
   router.post('/api/matchscore', function(req, res){
     if(req.body){
       const matchScore = new MatchScore(req.body.distance, req.body.score, req.body.xcount);
       matchScorePool[matchScore.uuid] = matchScore;
       return response(200, matchScore)(res);
-    // res.writeHead(200, {'Content-Type': 'application/json'});
-    // console.log('entered POST route: ', JSON.stringify(matchScore));
-    // console.log('matchScorePool: ', matchScorePool);
-    // res.write(JSON.stringify(matchScorePool[req.url.query.uuid]));
-    // res.end();
     }
     response(400, 'bad request')(res);
   }).get('/api/matchscore', function(req, res){
     if(matchScorePool[req.url.query.uuid]){
       const matchScore = matchScorePool[req.url.query.uuid];
       return response(200, matchScore)(res);
-      // res.writeHead(200, {'Content-Type': 'application/json'});
-      // res.write(JSON.stringify(matchScorePool[req.url.query.uuid]));
-      // res.end();
     }
     response(404, 'not found')(res);
   }).put('/api/matchscore', function(req, res){
@@ -34,21 +25,13 @@ module.exports = function(router){
       uuid.score= req.body.score;
       uuid.xCount= req.body.xCount;
       return response(200, uuid)(res);
-      // res.writeHead(200, {'Content-Type': 'application/json'});
-      // res.write(JSON.stringify(matchScorePool[req.url.query.uuid]));
-      // res.end();
     }
     response(404, 'not found')(res);
   }).delete('/api/matchscore', function(req, res){
     if(matchScorePool[req.body.uuid]){
       const uuid = matchScorePool[req.body.uuid];
-      // console.log('delete statement is true');
       delete matchScorePool[req.body.uuid];
       return response(404, uuid)(res);
-      // res.writeHead(200, {'Content-Type': 'application/json'});
-      // res.write('file deleted: ');
-      // console.log(matchScorePool);
-      // res.end();
     }
     response(404, 'not found')(res);
   });
