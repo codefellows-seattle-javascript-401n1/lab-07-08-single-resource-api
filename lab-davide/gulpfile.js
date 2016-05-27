d3199677f6b7cc9c30d818adfd4d6cbde88f56c1
FazioD <faziod@uw.edu>
2016-05-27 14:15:17 -0700

    resubmitting gulpfile

--- lab-davide/gulpfile.js
+++ lab-davide/gulpfile.js
@@ -1,59 +1,16 @@
 'use strict';
 
-const gulp = require('gulp');
+const gulp =  require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const nodemon = require('gulp-nodemon');
 
-//*.js will look for gulpfile and server.js//
 const paths = ['*./js', 'lib/*.js', 'model/*.js;', 'test/*.js', 'route/*.js'];
 
-//first item is always the name of the task, second thing can be an array of the dependencies of this task//
-gulp.task('eslint', function() {
-  return gulp.src(paths)
-    .pipe(eslint(
-      {
-        'rules': {
-          'no-console': 0,
-          'indent': [
-            2,
-            2
-          ],
-          'quotes': [
-            2,
-            'single'
-          ],
-          'linebreak-style': [
-            2,
-            'unix'
-          ],
-          'semi': [
-            2,
-            'always'
-          ]
-        },
-        'env': {
-          'es6': true,
-          'node': true,
-          'browser': true
-        },
-        'globals': {
-          'describe': false,
-          'it': false,
-          'beforeEach': false,
-          'afterEach': false,
-          'before': false,
-          'after': false
-        },
-        'ecmaFeatures': {
-          'modules': true,
-          'experimentalObjectRestSpread': true,
-          'impliedStrict': true
-        },
-        'extends': 'eslint:recommended'
-      }
-    ))
-    .pipe(eslint.format());
+
+gulp.task('eslint', function(){
+  gulp.src(paths)
+  .pipe(eslint());
 });
 
 gulp.task('test', function(){
@@ -63,8 +20,9 @@ gulp.task('test', function(){
 
 gulp.task('nodemon', function(){
   nodemon({
-    script: 'server.js',
-    ext: 'js'
+    script: 'server.js'
+  , ext: 'js.html'
+  , env: { 'NODE_ENV': 'deployment'}
   });
 });
 
