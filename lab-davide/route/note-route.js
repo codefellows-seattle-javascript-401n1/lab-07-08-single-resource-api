0b63d8dafd592c9944e509624364fc73102c8785
FazioD <faziod@uw.edu>
2016-06-14 08:06:33 -0700

    corrected/fixed routes

--- lab-davide/route/note-route.js
+++ lab-davide/route/note-route.js
@@ -2,58 +2,54 @@
 
 const Note = require('../model/note');
 const response = require('../lib/response');
-const storage = require('../lib/storage');
+const Storage = require('../lib/storage');
 
+//TODO: change this to dataDir and update storage//
+var storage = new Storage(`${__dirname}/../data`);
 
-//TODO: Add storage from notes and add dataDir//
-var storage = new Storage(dataDir);
-
+//POST 200 & 400//
 module.exports = function(router){
   router
   .post('/api/note', function(req, res){
-    if(req.body != undefined){
-      const note = new Note(req.body);
-      storage.setItem('notes', note).then(function(){
-        return response(200, note)(res);
-      }).catch(function(err){
-        console.error(err);
-      });
-    } else {
-      response(400, 'bad request')(res);
+    var note = new Note(req.body.content);
+    if(!req.body.content){
+      return response(400, 'bad request');
     }
-  })
-
-  .get('/api/note', function(req, res){
-    var note = req.url.query.id;
-    if (note) {
-      storage.fetchItem('notes', note).then(function(){
+    storage.setItem('notes', note).then(function(){
       return response(200, note)(res);
     }).catch(function(err){
-      console.error(err);
-      response(404, 'not found')(res);
+      console.log(err);
+      return response(400, 'bad request')(res);
     });
-  }
-    if(!req.url.query.id) {
-
-    }
-
   })
 
-  // bonus -->.put('/api/note', function(req, res){
-  //   const note = notePool[req.body.id];
-  //   //Check that the note exists
-    //Store the data somewhere
-    //Replace the old data with new data
-    //Return new data
 
+//GET 200 & 404//
+  .get('/api/note', function(req, res){
+    var note = req.url.query.id;
+    if (!req.url.id){
+      return response(400, 'bad request')(res);
+    }
+    console.log('working');
+    storage.fetchItem('notes', note).then(function(){
+      return response(200, note)(res);
+    }).catch(function(err){
+      err;
+      return response(404, 'not found')(res);
+    });
+  })
 
-  .delete('api/note', function(req, res){
-    if (req.body){
+//DELETE 200, 400, & 404//
+  .delete('/api/note', function(req, res){
     var note = req.body.id;
-    console.log(note);
+    if (!req.body.id){
+      return response(400, 'bad request')(res);
+    }
     storage.deleteItem('notes', note).then(function(){
       return response(200, 'note is deleted')(res);
-      console.error(err);
-      response(404, 'not found')(res);
+    }).catch(function(err){
+      err;
+      return response(404, 'not found')(res);
+    });
   });
 };
