e6781906f779f31bfd9a1c7ee15fcf57a87d56de
FazioD <faziod@uw.edu>
2016-07-04 13:10:25 -0700

    changes and refactoring

--- lab-davide/test/note-route-test.js
+++ lab-davide/test/note-route-test.js
@@ -1,19 +1,24 @@
-
 'use strict';
 
+const debug = require('debug')('note:note-route-test');
+
+//npm modules//
 const request = require('superagent');
 const expect = require('chai').expect;
 
+//app modules//
 const server = require('../server');
+
+//env variable//
 const port = process.env.PORT || 3000;
-const serverUrl = `http: //localhost:${port}`;
+const serverUrl = `http://localhost:${port}`;
 
 
-//testing the server is functional - up an running. environment is set up//
-describe('testing note-route module', function(){
+//testing the server is functional - up an running//
+describe('testing note-route module', function() {
   before(function(done) {
     if(!server.isRunning){
-      server.listen(port, function(){
+      server.listen(port, () => {
         done();
       });
       return;
@@ -21,9 +26,9 @@ describe('testing note-route module', function(){
     done();
   });
 
-  after(function(done){
-    if (server.isRunning){
-      server.close(function(){
+  after((done) => {
+    if (server.isRunning) {
+      server.close(() => {
         server.isRunning = false;
         done();
       });
@@ -32,87 +37,70 @@ describe('testing note-route module', function(){
     done();
   });
 
-//testing method POST//
-  describe('testing method POST on endpoint /api/note', function(){
-    before((done)=>{
-      console.log('serverUrl', serverUrl);
+  describe('testing POST on /api/note', function() {
+    debug('hitting POST api/note');
+    before((done) => {
       request.post(`${serverUrl}/api/note`)
-       .send({content: 'test note !!!'})
-       .end((err, res) => {
-         this.res = res;
-         this.note = res.body;
-         done();
-       });
-    });
-
-    it('should return status 200', ()=>{
-      request;
-      expect(this.res.status).to.equal(200);
-    });
-
-    it('should return a note', ()=>{
-      expect(this.note.content).to.equal('test note !!!');
-    });
-  });
-
-});
-
-  describe('testing method POST with no body', function(){
-    before((done)=>{
-      console.log(`${serverUrl}/api/note`, function(){
-      request.post
-      .send({content:""})
-      .end((err, res) =>{
-        this.res = res.
-        this.note = res.body;
-        done();
-      });
-  });
-
-      it('should return a status 400', ()=>{
-        request;
-        expect(this.res.status).to.equal(400);
-      });
-
-      it('should return \"bad request\"', ()=>{
-        expect(this.res.text).to.equal('bad request');
-      });
-    });
-
-//add in method get testing//
-  describe('testing method GET on endpoint /api/note', function(){
-    before((done)=>{
-      var id = '8675309';
-      console.log('serverUrl', serverUrl);
-      request.post(`${serverUrl}` + '/api/note/?id == 8675309', id)
-       .send()
-       .end((err, res) => {
-         this.res = res;
-         this.note = res.body;
-         done();
-       });
-    });
-
-    it('it should return a status 404', ()=>{
-      request;
-      expect(this.res.status).to.equal(404);
-    });
-
-    it('should return id \"not found\"', ()=>{
-      expect(this.res.text).to.equal('not found');
-
-    });
-
-    describe('testing method GET with no id', function(){
-      before((done)=>{
-        var id = '';
-        console.log('serverUrl', serverUrl);
-        request.post(`${serveUrl}` + '/api/note/id == ', id)
-        .send()
+      .send({content: 'test note' })
         .end((err, res) => {
           this.res = res;
           this.note = res.body;
           done();
         });
-      })
-    })
+    });
+
+    it('should return a note', () => {
+      expect(this.res.status).to.equal(200);
+      expect(this.note.content).to.equal('test note');
+    });
+
+    it('should return a bad request', () => {
+      request.post(`${serverUrl}/api/note/`)
+      .end((res) => {
+        expect(res.status).to.equal(400);
+      });
+    });
+  });
+
+  describe('testing GET on api/note', function () {
+    debug('hitting GET api/note');
+    before((done) => {
+      request.post(`${serverUrl}/api/note`)
+      .send({content: 'test note'})
+      .end((err, res) => {
+        this.res = res;
+        request.get(`${serverUrl}/api/note/?id= 8675309`)
+        .end((err, res) => {
+          this.res = res;
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+
+    it('should return a note', () => {
+      expect(this.res.status).to.equal(404);
+    });
+
+    describe('testing GET on api/note', function () {
+      debug('hitting GET ');
+      before((done) => {
+        request.post(`${serverUrl}/api/note`)
+        .send({content: 'test note'})
+        .end((err, res) => {
+          this.res = res;
+          request.get(`${serverUrl}/api/note`)
+          .end((err, res) => {
+            this.res = res;
+            expect(res.status).to.equal(400);
+            done();
+          });
+        });
+      });
+
+      it('should return not found', () => {
+        expect(this.res.status).to.equal(400);
+      });
+    });
+  });
+});
